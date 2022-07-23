1635f518f286affe7979eded83f1554856cf8642
Chris Follen <christopher.follen@gmail.com>
2022-07-23 12:20:01 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const testObj = {
-    key1: 'thing',
-    b: '789243',
-    c: 'a longer string goes here then wut happens'
-  };
-
-  console.log('hello');
-
-  console.log('things');
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. will this eventually
-          wrap around if this line gets long enough
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello, world!</p>
     </div>
   );
 }
